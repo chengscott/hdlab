459a61f97b94956c4b329282076c79571736f762
chengscott <chengscott@live.com>
2016-10-09 12:53:16 +0800

    add lab3

---
+++ lab3_1/lab3_1.runs/impl_1/lab3_1.tcl
@@ -0,0 +1,65 @@
+proc start_step { step } {
+  set stopFile ".stop.rst"
+  if {[file isfile .stop.rst]} {
+    puts ""
+    puts "*** Halting run - EA reset detected ***"
+    puts ""
+    puts ""
+    return -code error
+  }
+  set beginFile ".$step.begin.rst"
+  set platform "$::tcl_platform(platform)"
+  set user "$::tcl_platform(user)"
+  set pid [pid]
+  set host ""
+  if { [string equal $platform unix] } {
+    if { [info exist ::env(HOSTNAME)] } {
+      set host $::env(HOSTNAME)
+    }
+  } else {
+    if { [info exist ::env(COMPUTERNAME)] } {
+      set host $::env(COMPUTERNAME)
+    }
+  }
+  set ch [open $beginFile w]
+  puts $ch "<?xml version=\"1.0\"?>"
+  puts $ch "<ProcessHandle Version=\"1\" Minor=\"0\">"
+  puts $ch "    <Process Command=\".planAhead.\" Owner=\"$user\" Host=\"$host\" Pid=\"$pid\">"
+  puts $ch "    </Process>"
+  puts $ch "</ProcessHandle>"
+  close $ch
+}
+
+proc end_step { step } {
+  set endFile ".$step.end.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+proc step_failed { step } {
+  set endFile ".$step.error.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+set_msg_config -id {HDL 9-1061} -limit 100000
+set_msg_config -id {HDL 9-1654} -limit 100000
+
+start_step write_bitstream
+set rc [catch {
+  create_msg_db write_bitstream.pb
+  open_checkpoint lab3_1_routed.dcp
+  set_property webtalk.parent_dir /media/scott/BC74E09A74E058A2/hdlab/lab3_1/lab3_1.cache/wt [current_project]
+  catch { write_mem_info -force lab3_1.mmi }
+  write_bitstream -force lab3_1.bit 
+  catch { write_sysdef -hwdef lab3_1.hwdef -bitfile lab3_1.bit -meminfo lab3_1.mmi -file lab3_1.sysdef }
+  catch {write_debug_probes -quiet -force debug_nets}
+  close_msg_db -file write_bitstream.pb
+} RESULT]
+if {$rc} {
+  step_failed write_bitstream
+  return -code error $RESULT
+} else {
+  end_step write_bitstream
+}
+
---
+++ lab3_2/lab3_2.srcs/sources_1/new/lab3_2.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+
+module lab3_2(
+input clk,
+input reset,
+input sel_clk,
+output reg [15:0] LED);
+// state
+parameter [1:0] LL = 2'b00,
+                LR = 2'b01,
+                RL = 2'b10,
+                RR = 2'b11;
+// constant
+parameter [15:0] L_init = 16'b0000_0001_0000_0000,
+                 L_full = 16'b1111_1111_0000_0000,
+                 R_init = 16'b0000_0000_1000_0000,
+                 R_full = 16'b0000_0000_1111_1111;
+reg [1:0] state, state_next;
+reg [15:0] LED_next;
+
+clock_divider #(26) cd26(.clk(clk), .clk_div(clk26));
+clock_divider #(19) cd19(.clk(clk), .clk_div(clk19));
+
+wire nclk = sel_clk ? clk19 : clk26;
+
+always @(posedge nclk or posedge reset) begin
+    if (reset == 1) begin
+        LED <= 0;
+        state <= LL;
+    end else begin
+        LED <= LED_next;
+        state <= state_next;
+    end
+end
+
+always @* begin
+    state_next = state;
+    case (state)
+        LL: begin
+            LED_next = LED == 0 ? L_init : LED | (LED << 1);
+            if (LED_next == L_full)
+                state_next = LR;
+        end
+        LR: begin
+            LED_next = LED & (LED >> 1);
+            if (LED_next == 0)
+                state_next = RR;
+        end
+        RR: begin
+            LED_next = LED == 0 ? R_init : LED | (LED >> 1);
+            if (LED_next == R_full)
+                state_next = RL;
+        end
+        RL: begin
+            LED_next = LED & (LED << 1);
+            if (LED_next == 0)
+               state_next = LL;
+        end
+    endcase
+end
+
+endmodule
