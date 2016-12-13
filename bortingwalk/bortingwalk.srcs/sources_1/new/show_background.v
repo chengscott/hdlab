dae3c5c5c219e8842073adf4ad15abd3dd1f2c23
adamatuno <adam0923686343@yahoo.com.tw>
2016-12-13 17:23:29 +0800

    add show_background

---
+++ bortingwalk/bortingwalk.srcs/sources_1/new/show_background.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+module show_background(
+    input clk,
+    input rst,
+    output [3:0] vgaRed,
+    output [3:0] vgaGreen,
+    output [3:0] vgaBlue,
+    output hsync,
+    output vsync
+    );
+endmodule
