08d4e9cf84e6aa3ee9976b91663875c559b9bdac
adamatuno <adam0923686343@yahoo.com.tw>
2016-12-20 17:35:07 +0800

    show road finish

---
+++ bortingwalk/bortingwalk.srcs/sources_1/new/count_backward.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2016/12/20 16:02:37
+// Design Name: 
+// Module Name: count_backward
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module count_backward(
+
+    );
+endmodule
