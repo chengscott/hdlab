9be5f410e26af44cb11d7b437b82fe62ad5698c3
adamatuno <adam0923686343@yahoo.com.tw>
2016-12-22 01:02:44 +0800

    show_background finish

--- bortingwalk/bortingwalk.srcs/sources_1/new/count_backward.v
+++ bortingwalk/bortingwalk.srcs/sources_1/new/count_backward.v
@@ -1,26 +1,8 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2016/12/20 16:02:37
-// Design Name: 
-// Module Name: count_backward
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module count_backward(
-
+    input clk,
+    input rst,
+    output [3:0]DIGIT,
+    output [6:0]DISPLAY
     );
 endmodule
--- bortingwalk/bortingwalk.srcs/sources_1/new/pixel_gen.v
+++ bortingwalk/bortingwalk.srcs/sources_1/new/pixel_gen.v
@@ -5,13 +5,20 @@ module pixel_gen(
    output reg [3:0] vgaGreen,
    output reg [3:0] vgaBlue
    );
+   parameter [11:0] ln = 12'hff0;
+   parameter [11:0] bg = 12'h888;
    
        always @(*) begin
-       if (v_cnt<=163 && v_cnt>=157)
-            {vgaRed, vgaGreen, vgaBlue} = 12'hff0;
-       else if (v_cnt<=323 && v_cnt>=317)
-            {vgaRed, vgaGreen, vgaBlue} = 12'hff0;
-       else 
-            {vgaRed, vgaGreen, vgaBlue} = 12'h222;
+       if(!valid)
+            {vgaGreen, vgaRed, vgaBlue} = 12'h0;
+       else if (v_cnt <= 163 && v_cnt >= 157)
+            {vgaGreen, vgaRed, vgaBlue} = ln;
+       else if (v_cnt <= 323 && v_cnt >= 317)
+            {vgaGreen, vgaRed, vgaBlue} = ln;
+       else if (v_cnt <= 480)
+            {vgaGreen, vgaRed, vgaBlue} = bg;
+       else
+            {vgaGreen, vgaRed, vgaBlue} = 12'b0;
    end
 endmodule
+
