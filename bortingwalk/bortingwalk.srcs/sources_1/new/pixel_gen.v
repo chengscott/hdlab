d28baea0bb97036b0326d0f1a45885cd138f0eb3
chengscott <chengscott@live.com>
2016-12-27 15:33:22 +0800

    add car proto

--- bortingwalk/bortingwalk.srcs/sources_1/new/pixel_gen.v
+++ bortingwalk/bortingwalk.srcs/sources_1/new/pixel_gen.v
@@ -2,15 +2,18 @@ module pixel_gen #(
     parameter [11:0] line = 12'hff0,
                      bg = 12'h888,
     parameter [9:0] BORTING_X = 10'd140,
-                    BORTING_Y = 10'd160
+                    BORTING_Y = 10'd160,
+                    CAR_X = 10'd140,
+                    CAR_Y = 10'd130
 ) (
    input clk, valid,
    input [9:0] h_cnt, v_cnt,
-   input [9:0] borting_x, borting_y,
+   input [9:0] borting_x, borting_y, car_x, car_y,
    output reg [3:0] vgaRed, vgaGreen, vgaBlue
 );
-wire [14:0] borting_addr = (((h_cnt - borting_x)>>1) + BORTING_X*((v_cnt- borting_y)>>1))%11280;
-wire [11:0] borting_px;
+wire [14:0] borting_addr = (((h_cnt - borting_x)>>1) + BORTING_X*((v_cnt - borting_y)>>1))%22400;
+wire [14:0] car_addr = (((h_cnt - car_x)>>1) + BORTING_X*((v_cnt - car_y)>>1))%11280;
+wire [11:0] borting_px, car_px;
 
 always @* begin
    if (!valid) {vgaGreen, vgaRed, vgaBlue} = 12'h0;
@@ -40,5 +43,13 @@ borting_mem borting(
   .dina(0),
   .douta(borting_px)
 );
-
+/*
+car_mem car(
+  .clka(clk),
+  .wea(0),
+  .addra(car_addr),
+  .dina(0),
+  .douta(car_px)
+);
+*/
 endmodule
--- bortingwalk/bortingwalk.srcs/sources_1/new/render_scene.v
+++ bortingwalk/bortingwalk.srcs/sources_1/new/render_scene.v
@@ -3,7 +3,9 @@
 module render_scene #(
     parameter [9:0] DELTA = 10'd10,
                     BORTING_X = 10'd140,
-                    BORTING_Y = 10'd160
+                    BORTING_Y = 10'd160,
+                    CAR_X = 10'd140,
+                    CAR_Y = 10'd130
 ) (
     input clk, rst,
     input kR, kW, kA, kS, kD, k0, k1, k2, k3, ksp,
@@ -11,21 +13,27 @@ module render_scene #(
     output hsync, vsync
 );
 wire [9:0] h_cnt, v_cnt; // 640, 480
-reg [9:0] borting_x, borting_y;
-wire [9:0] borting_x_next, borting_y_next;
+reg [9:0] borting_x, borting_y, car_x, car_y;
+wire [9:0] borting_x_next, borting_y_next, car_x_next, car_y_next;
 
 assign borting_x_next = borting_x + (kD ? DELTA : (kA ? -DELTA : 0));
 assign borting_y_next = borting_y + (kW ? -DELTA : (kS ? DELTA : 0));
+assign car_x_next = car_x;
+assign car_y_next = car_y;
 
 clock_divider #(2) cd25Mz(.clk(clk), .clk_div(clk_25MHz));
 clock_divider #(16) cdlp(.clk(clk), .clk_div(clklp));
 
 pixel_gen #(
-    .BORTING_X(BORTING_X),
-    .BORTING_Y(BORTING_Y)
+   .BORTING_X(BORTING_X),
+   .BORTING_Y(BORTING_Y),
+   .CAR_X(CAR_X),
+   .CAR_Y(CAR_Y)
 ) pixel_ctr(
    .borting_x(borting_x),
    .borting_y(borting_y),
+   .car_x(car_x),
+   .car_y(car_y),
    .clk(clk),
    .h_cnt(h_cnt),
    .v_cnt(v_cnt),
@@ -49,9 +57,13 @@ always @(posedge clklp) begin
     if (rst) begin
         borting_x = 0;
         borting_y = 10'd163;
+        car_x = -CAR_X;
+        car_y = 10'd10;
     end else begin
         borting_x = (borting_x_next >= 0 && borting_x_next + BORTING_X < 640) ? borting_x_next : borting_x;
         borting_y = (borting_y_next >= 0 && borting_y_next + BORTING_Y < 480) ? borting_y_next : borting_y;
+        car_x = car_x_next;
+        car_y = car_y_next;
     end
 end
 
